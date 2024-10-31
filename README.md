baa80cc078208b721d7e50bc578f469c9b65cc9f
Krystian Nowak <kr21032002@icloud.com>
2024-10-31 12:46:29 -0700

    cleanup

--- README.md
+++ README.md
@@ -16,6 +16,18 @@ To compile run make
 make
 ```
 
+Then, you can simply call `mish` in either the interactive mode or with file.
+
+```sh
+./mish 
+```
+
+or 
+
+```sh
+./mish some_file.txt
+```
+
 This will build the mish for you. Then to run the predefined tests from ostep-project:
 
 ```sh
@@ -23,4 +35,35 @@ chmod +x run-tests.sh
 ./run-test.sh
 ```
 
+I have also defined some unit tests for the purpose of testing my own code. To run those test, you can use make
 
+```sh
+make tests
+make run_tests
+```
+
+To clean the directory from executables and object file, you can run 
+
+```sh
+make clean
+```
+
+which will build and run unit tests for `builtins` and `parser`.
+
+## AI Statement
+
+I have used AI heavily to explain the syntax and inner workings of C. Example prompts include, but are not limited to: 
+
+* How to define `struct` in C?
+* What is the difference between `typedef struct` and `struct`?
+* How to define a pointer to an arbitrary data type?
+* What's the difference between using `strcmp` and using `==`?
+* How to use file descriptors for reading in arguments from cli?
+* How to replace a character with a string so that the character appears empty? (question about \0)
+* How to use `fork()` to run multiple processes conccurently?
+
+I have also used AI to debug my compiler statements. For example, I might use AI to ask the following: 
+
+*Given the error below, can you explain me what does the problem relates to? What could be possible causes? error here*
+
+Last, but not least, I have also used AI to help me write the Makefile. I have defined the source path, unit test files, and source for objects, and asked AI to write the Makefile for building different targets.
--- src/execute.c
+++ src/execute.c
@@ -11,79 +11,94 @@
 // executes a command
 // returns 0 if everything is OK
 // returns 1 if an error occurred
-int execute_command(char **args, int background, pid_t *pid_out) {
-  if (args[0] == NULL) {
+int execute_command(char **args, int background, pid_t *pid_out)
+{
+  if (args[0] == NULL)
+  {
     // if command is empty return error
     return 1;
   }
 
   // from test 12: first argument cannot be a redirection operator
   // check if the first token is a redirection operator
-  if (strcmp(args[0], ">") == 0) {
+  if (strcmp(args[0], ">") == 0)
+  {
     write(STDERR_FILENO, error_message, strlen(error_message));
     return 1;
   }
 
   // check for built-in commands
-  if (strcmp(args[0], "exit") == 0) {
+  if (strcmp(args[0], "exit") == 0)
+  {
     return cmd_exit(args);
-  } else if (strcmp(args[0], "cd") == 0) {
+  }
+  else if (strcmp(args[0], "cd") == 0)
+  {
     return cmd_cd(args);
-  } else if (strcmp(args[0], "path") == 0) {
+  }
+  else if (strcmp(args[0], "path") == 0)
+  {
     return cmd_path(args);
   }
 
-  // Handle redirection
+  // handle redirection
   char *output_file = NULL;
   int redirect_count = 0;
   int i = 0;
-  while (args[i] != NULL) {
-    if (strcmp(args[i], ">") == 0) {
+  while (args[i] != NULL)
+  {
+    if (strcmp(args[i], ">") == 0)
+    {
       redirect_count++;
-      if (redirect_count > 1) {
-        // Multiple redirection operators
+      if (redirect_count > 1)
+      {
+        // multiple redirection operators
         write(STDERR_FILENO, error_message, strlen(error_message));
         return 1;
       }
-      if (args[i + 1] == NULL) {
-        // Missing output file
+      if (args[i + 1] == NULL)
+      {
+        // missing output file
         write(STDERR_FILENO, error_message, strlen(error_message));
         return 1;
       }
       output_file = args[i + 1];
-      args[i] = NULL; // Terminate the args before '>'
+      args[i] = NULL; // terminate the args before '>'
       break;
     }
     i++;
   }
 
-  // After handling redirection, ensure no extra arguments exist
-  if (args[i + 2] != NULL) {
-    // Extra arguments after output file
+  // after handling redirection, ensure no extra arguments exist
+  if (args[i + 2] != NULL)
+  {
+    // extra arguments after output file
     write(STDERR_FILENO, error_message, strlen(error_message));
     return 1;
   }
-
-  // Launch external program
   return launch_program(args, background, output_file, pid_out);
 }
 
 int launch_program(char **args, int background, char *output_file,
-                   pid_t *pid_out) {
+                   pid_t *pid_out)
+{
   pid_t pid;
   int status;
   char executable_path[1024];
   int found = 0;
 
   pid = fork();
-  if (pid == 0) {
+  if (pid == 0)
+  {
     // child process
 
     // handle output redirection
-    if (output_file != NULL) {
+    if (output_file != NULL)
+    {
       // open the output file
       int fd = open(output_file, O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU);
-      if (fd < 0) {
+      if (fd < 0)
+      {
         // error opening the file
         write(STDERR_FILENO, error_message, strlen(error_message));
         exit(1);
@@ -96,11 +111,13 @@ int launch_program(char **args, int background, char *output_file,
     }
 
     // search for executable in the path directories
-    for (int i = 0; i < path_count; i++) {
+    for (int i = 0; i < path_count; i++)
+    {
       // construct the full path to the executable
       snprintf(executable_path, sizeof(executable_path), "%s/%s", path[i],
                args[0]);
-      if (access(executable_path, X_OK) == 0) {
+      if (access(executable_path, X_OK) == 0)
+      {
         found = 1;
         execv(executable_path, args);
         // if execv returns, an error occurred
@@ -108,19 +125,25 @@ int launch_program(char **args, int background, char *output_file,
         exit(1);
       }
     }
-    if (!found) {
+    if (!found)
+    {
       // command not found in any path
       write(STDERR_FILENO, error_message, strlen(error_message));
       exit(1);
     }
-  } else if (pid < 0) {
+  }
+  else if (pid < 0)
+  {
     // fork failed
     write(STDERR_FILENO, error_message, strlen(error_message));
     return 1;
-  } else {
+  }
+  else
+  {
     // parent process
     *pid_out = pid;
-    if (!background) {
+    if (!background)
+    {
       waitpid(pid, &status, 0);
     }
   }
